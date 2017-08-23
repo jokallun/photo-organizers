121685469715fecc7117772c96b647fd865d7e2b
Jouni Kallunki <jouni.kallunki@reaktor.com>
2017-08-23 10:25:56 +0300

    add delete-archives task, reformat datetime strings

--- glacier_inventory.py
+++ glacier_inventory.py
@@ -43,7 +43,7 @@ def get_vault_inventory(ctx, job_file, account_id=None, vault_name=None, region=
         inventory_response = job.get_output()
         inventory_json = inventory_response['body'].read()
         inventory = json.loads(inventory_json)
-        dt_str = datetime.today().strftime('%Y-%m-%d')
+        dt_str = datetime.today().strftime('%Y%m%d-%H%M')
         fname = 'inventory-{}.json'.format(dt_str)
         logger.info('Writing inventory to file {}'.format(fname))
         with open(fname, 'w') as f:
@@ -70,10 +70,33 @@ def initiate_vault_inventory(ctx, account_id=None, vault_name=None, region=None)
     print(vault_name)
     vault = glacier.Vault(account_id, vault_name)
     job = vault.initiate_inventory_retrieval()
-    dt_str = datetime.today().strftime('%Y-%m-%d')
+    dt_str = datetime.today().strftime('%Y%m%d-%H%M')
     fname = 'inventory-job-{}.json'.format(dt_str)
     with open(fname, 'w') as f:
         json.dump({'account_id': job.account_id,
                    'vault_name': job.vault_name, 'id': job.id}, f)
     logger.info('Writing vault inventory job info to {}'.format(fname))
     return job
+
+@task
+def delete_archives(ctx, file_list, account_id=None, vault_name=None, region=None):
+    """Delete archives listed in file_list
+
+    file-list is an json file returned by get-vault-inventory
+    or with similar structure
+
+    Other  argments are optional and default to values in photo-organizer.json
+    """
+    account_id = account_id or ctx.config.glacier.account_id
+    vault_name = vault_name or ctx.config.glacier.vault_name
+    region = region or ctx.config.glacier.region
+    logger.info('Initiating inventory for {}'.format(vault_name))
+    glacier = boto3.resource('glacier', region_name=region)
+    vault = glacier.Vault(account_id, vault_name)
+    archive_infos = json.load(open(file_list))
+    for n, info in enumerate(archive_infos.get('ArchiveList')):
+        archive = vault.Archive(info['ArchiveId'])
+        archive.delete()
+        logger.info('deleted {}'.format(info['ArchiveId']))
+        if n % 100 == 0:
+            logger.info('Deleted {} archives'.format())
